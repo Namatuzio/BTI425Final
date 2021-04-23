d054a0b22d9495d304ef9281d75510e800835bfe
Nick A <nictuzio@gmail.com>
2021-04-22 23:36:32 -0400

    commit

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -8,21 +8,22 @@ import { NgForm } from '@angular/forms';
 })
 export class ContactUsComponent implements OnInit {
 
-  public name: String;
-  public email: String;
-  public message: String;
-  public warning: String;
+
   @Input() submit: boolean = false;
   @Output() out: EventEmitter<Boolean> = new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  public name: String;
+  public email: String;
+  public message: String;
+  public warning: String;
   onSubmit(f: NgForm): void {
-
-
-    if(this.name == null || this.email == null || this.message == null)
+    this.name = f.value.userName;
+    this.email = f.value.email;
+    this.message = f.value.message;
+    if(this.name == null || this.email == null || this.message == null || this.name == "" || this.email == "" || this.message == "")
     {
       this.warning = "The fields can not be empty";
     }
